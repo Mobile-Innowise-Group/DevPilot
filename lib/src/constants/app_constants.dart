fd5cb62e7c3f9575bbe656f1a27bfc1e9bf3e3f3
German Perelmuter <german.perelmuter@innowise.com>
2024-08-06 15:55:25 +0200

    Navigation, localization integration, dependency update

--- lib/src/constants/app_constants.dart
+++ lib/src/constants/app_constants.dart
@@ -135,6 +135,7 @@ void main() {
 
   static const String kMainCommonContent = '''
 import 'package:core/core.dart';
+import 'package:core_ui/core_ui.dart';
 import 'package:data/data.dart';
 import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
@@ -166,8 +167,26 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const AppErrorHandlerProvider(
-      child: Placeholder(),
+    final AppRouter appRouter = appLocator<AppRouter>();
+
+    return EasyLocalization(
+      path: AppLocalization.langFolderPath,
+      supportedLocales: AppLocalization.supportedLocales,
+      fallbackLocale: AppLocalization.fallbackLocale,
+      child: Builder(
+        builder: (BuildContext context) {
+          return AppErrorHandlerProvider(
+            child: MaterialApp.router(
+              debugShowCheckedModeBanner: false,
+              routerConfig: appRouter.config(),
+              localizationsDelegates: context.localizationDelegates,
+              supportedLocales: context.supportedLocales,
+              locale: context.locale,
+              theme: lightTheme,
+            ),
+          );
+        },
+      ),
     );
   }
 }
